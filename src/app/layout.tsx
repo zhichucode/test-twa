543f18105f919d54dac2ac5fa5e63a3b993410bd
Hoppe <43780800+zhichucode@users.noreply.github.com>
2024-11-29 14:46:54 +0800

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { PropsWithChildren } from 'react';
+import type { Metadata } from 'next';
+import { getLocale } from 'next-intl/server';
+
+import { Root } from '@/components/Root/Root';
+import { I18nProvider } from '@/core/i18n/provider';
+
+import '@telegram-apps/telegram-ui/dist/styles.css';
+import 'normalize.css/normalize.css';
+import './_assets/globals.css';
+
+export const metadata: Metadata = {
+  title: 'Your Application Title Goes Here',
+  description: 'Your application description goes here',
+};
+
+export default async function RootLayout({ children }: PropsWithChildren) {
+  const locale = await getLocale();
+
+  return (
+    <html lang={locale}>
+    <body>
+      <I18nProvider>
+        <Root>
+          {children}
+        </Root>
+      </I18nProvider>
+    </body>
+    </html>
+  );
+}
---
+++ src/components/Root/Root.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import { type PropsWithChildren, useEffect } from 'react';
+import {
+  initData,
+  miniApp,
+  useLaunchParams,
+  useSignal,
+} from '@telegram-apps/sdk-react';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import { AppRoot } from '@telegram-apps/telegram-ui';
+
+import { ErrorBoundary } from '@/components/ErrorBoundary';
+import { ErrorPage } from '@/components/ErrorPage';
+import { useTelegramMock } from '@/hooks/useTelegramMock';
+import { useDidMount } from '@/hooks/useDidMount';
+import { useClientOnce } from '@/hooks/useClientOnce';
+import { setLocale } from '@/core/i18n/locale';
+import { init } from '@/core/init';
+
+import './styles.css';
+
+function RootInner({ children }: PropsWithChildren) {
+  const isDev = process.env.NODE_ENV === 'development';
+
+  // Mock Telegram environment in development mode if needed.
+  if (isDev) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useTelegramMock();
+  }
+
+  const lp = useLaunchParams();
+  const debug = isDev || lp.startParam === 'debug';
+
+  // Initialize the library.
+  useClientOnce(() => {
+    init(debug);
+  });
+
+  const isDark = useSignal(miniApp.isDark);
+  const initDataUser = useSignal(initData.user);
+
+  // Set the user locale.
+  useEffect(() => {
+    initDataUser && setLocale(initDataUser.languageCode);
+  }, [initDataUser]);
+
+  // Enable debug mode to see all the methods sent and events received.
+  useEffect(() => {
+    debug && import('eruda').then((lib) => lib.default.init());
+  }, [debug]);
+
+  return (
+    <TonConnectUIProvider manifestUrl="/tonconnect-manifest.json">
+      <AppRoot
+        appearance={isDark ? 'dark' : 'light'}
+        platform={['macos', 'ios'].includes(lp.platform) ? 'ios' : 'base'}
+      >
+        {children}
+      </AppRoot>
+    </TonConnectUIProvider>
+  );
+}
+
+export function Root(props: PropsWithChildren) {
+  // Unfortunately, Telegram Mini Apps does not allow us to use all features of
+  // the Server Side Rendering. That's why we are showing loader on the server
+  // side.
+  const didMount = useDidMount();
+
+  return didMount ? (
+    <ErrorBoundary fallback={ErrorPage}>
+      <RootInner {...props}/>
+    </ErrorBoundary>
+  ) : <div className="root__loading">Loading</div>;
+}
